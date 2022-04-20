6316899306a2c75e86d94e3c455c97483607df9c
FHSWar <fhswar0504@163.com>
2022-04-20 17:38:26 +0800

    feat: 干净的微前端demo

---
+++ react16/build/precache-manifest.6fa116c7421e2784a1f790c435b27d86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219c90ad1f1026a9a416aaeef2ff6e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca50e7eda54f77b438a",
+    "url": "/static/css/2.4eb19277.chunk.css"
+  },
+  {
+    "revision": "621d4a98dac9b4cdc214",
+    "url": "/static/css/main.c9be9530.chunk.css"
+  },
+  {
+    "revision": "aca50e7eda54f77b438a",
+    "url": "/static/js/2.d1e18e95.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d1e18e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633559e0d69ff56a46b1",
+    "url": "/static/js/3.6d6c1bb4.chunk.js"
+  },
+  {
+    "revision": "621d4a98dac9b4cdc214",
+    "url": "/static/js/main.1c07afd7.chunk.js"
+  },
+  {
+    "revision": "cf35d0194a74a033bb08",
+    "url": "/static/js/runtime-main.f55c3184.js"
+  }
+]);
